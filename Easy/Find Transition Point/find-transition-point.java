ed9ae7ea1a43ae1088e97351034d4f93f17647f0
Sanjeev Singh Bhadouria <99323593+Sanjeev98singh@users.noreply.github.com>
2024-02-21 17:00:31 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find Transition Point/find-transition-point.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Sorted_Array {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            Solution obj = new Solution();
+            System.out.println(obj.transitionPoint(arr, n));
+            T--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    int transitionPoint(int arr[], int n) {
+          if (arr[0] == 1) return 0;
+        if (arr[n - 1] == 0) return -1;
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == 1 && (mid == 0 || arr[mid - 1] == 0)) {
+                return mid;
+            } else if (arr[mid] == 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
